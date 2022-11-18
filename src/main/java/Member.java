6d0e13e26fdb9f3f0eec736d071c61229e10f31a
navidmirzad <navi0221@stud.kea.dk>
2022-11-18 14:16:15 +0100

    added empty UI, Controller, FileHandler, Database, Member;

---
+++ src/main/java/Member.java
@@ -0,0 +1,23 @@
+public class Member {
+
+    private String name;
+    private String lastname;
+    private int age;
+    private boolean membershipIsActive;
+    private boolean ageIsOver18;
+    private boolean swimsCompetitively;
+
+    public Member(String name, String lastname, int age,
+                  boolean membershipIsActive, boolean ageIsOver18, boolean swimsCompetitively) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+        this.membershipIsActive = membershipIsActive;
+        this.ageIsOver18 = ageIsOver18;
+        this.swimsCompetitively = swimsCompetitively;
+    }
+
+
+
+
+}
